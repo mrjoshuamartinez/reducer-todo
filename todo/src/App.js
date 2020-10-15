9c77ac53cb20f01f3ba4838bd94b1765fdc1fe70
Joshua Martinez <mrjoshuamartinez@gmail.com>
2020-10-15 00:37:34 -0400

    mvp

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useReducer } from 'react';
+import { Reducer } from './reducers/reducer';
 import './App.css';
 
 function App() {
-  
+  const [{ todos, todoCount }, dispatch] = useReducer(Reducer, { 
+    todos: [], 
+    todoCount: 0 
+  });
+  const [text, setText] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <div>ToDo List</div>
+        <div>ToDo List [ <span style={{color: '#61dafb', margin: '0px', padding: '0px'}}>{ todoCount }</span> ]</div>
         <div>
-
+          { todos.map((todo, index) => (
+              <div 
+              key={ index} 
+              onClick={() => dispatch(
+                { type: "TOGGLE_TODO", index }
+              )}
+              style={{
+                fontFamily: 'Tahoma',
+                fontSize: '1.5rem',
+                textDecoration: todo.completed ? 'line-through' : "",
+                color: todo.completed ? '#61dafb' : 'dimgray',
+                cursor: 'pointer'
+              }}
+              >
+                { todo.text }
+              </div>
+            )) 
+          }
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              dispatch({ type: "ADD_TODO", text });
+              setText("");
+            }}
+          >
+            <input 
+              type="text"
+              name="input"
+              value={ text }
+              onChange={e => setText(e.target.value)}
+            /><br />
+            <button>
+              Add
+            </button>
+          </form>
+          <button onClick={() => {dispatch({ type: "REMOVE_TODO" })}}>
+            Clear Completed
+          </button>
         </div>
       </header>
     </div>
--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,9 +1,23 @@
-import React from 'react';
-
-export const Reducer = () => {
-    return(
-        <>
-        
-        </>
-    );
-};
+export const Reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return { 
+                todos: [...state.todos, { text: action.text, completed: false, id: Date.now() }],
+                todoCount: state.todoCount + 1
+            };
+        case 'TOGGLE_TODO':
+            return { 
+                todos: state.todos.map((todo, index) => index === action.index ? { ...todo, completed: !todo.completed } : todo),
+                todoCount: state.todoCount
+            };
+        case 'REMOVE_TODO':
+            return {
+                todos: state.todos.filter(t => !t.completed),
+                todoCount: state.todos.length
+                // console.log(state)
+                // state.todos.filter(({index}) => index !== action.id);
+                }
+        default:
+            return state;
+    };
+ };
